030452d47b152251ff75dbb6d4569c407315d765
Leo June <leojunebedeo@gmail.com>
2022-10-04 13:37:54 +0800

    BD-10 Add soft delete to users

--- Modules/Users/Database/Migrations/2022_10_03_025829_create_users_table.php
+++ Modules/Users/Database/Migrations/2022_10_03_025829_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->string('avatar')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
