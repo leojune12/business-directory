3c8aaca16b598c90c3147329a6136701b957f0c5
Leo June <leojunebedeo@gmail.com>
2022-10-09 23:00:13 +0800

    BD-18 Add product module

---
+++ Modules/Product/Entities/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\Product\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Businesses\Entities\Business;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'business_id',
+        'name',
+        'description',
+        'price',
+        'is_available',
+    ];
+
+    // protected $guarded = [];
+
+    protected static function newFactory()
+    {
+        return \Modules\Product\Database\factories\ProductFactory::new();
+    }
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
+}
