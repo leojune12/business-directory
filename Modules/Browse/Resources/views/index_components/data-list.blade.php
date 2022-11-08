718612a248fff24c8a16e389049fb8d8b767ac74
Leo June <leojunebedeo@gmail.com>
2022-11-09 00:31:28 +0800

    Update browse data-list

--- Modules/Browse/Resources/views/index_components/data-list.blade.php
+++ Modules/Browse/Resources/views/index_components/data-list.blade.php
@@ -17,13 +17,15 @@
                         <div class="md:tw-text-lg tw-text-sm tw-text-black tw-font-medium tw-truncate tw-w-40 sm:tw-w-72 md:tw-w-80 xl:tw-w-96">
                             @{{ item.name }}
                         </div>
-                        <div class="tw-text-sm tw-text-gray-500 tw-flex tw-truncate tw-w-40 sm:tw-w-72 md:tw-w-80 xl:tw-w-96 -tw-ml-1">
+                        <div class="tw-text-sm tw-text-gray-500 tw-flex -tw-ml-1">
                             <span>
                                 <svg style="width:16px;height:16px" viewBox="0 0 24 24">
                                     <path fill="currentColor" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
                                 </svg>
                             </span>
-                            @{{ item.address }}
+                            <div class="tw-truncate tw-w-40 sm:tw-w-72 md:tw-w-80 xl:tw-w-96">
+                                @{{ item.address }}
+                            </div>
                         </div>
                     </div>
                 </div>
