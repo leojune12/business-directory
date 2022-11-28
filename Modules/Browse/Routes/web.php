c26453af77de94a4edddccf9c9b3c96ff269230a
Leo June <leojunebedeo@gmail.com>
2022-11-28 19:08:08 +0800

    Add products route

--- Modules/Browse/Routes/web.php
+++ Modules/Browse/Routes/web.php
@@ -2,18 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Browse\Services\BrowseService;
+use Modules\Browse\Http\Controllers\BrowseProductController;
 use Modules\Browse\Http\Controllers\BrowseBusinessController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 // Route::group(['middleware' => ['role:admin']], function () {
 
 //     Route::resource('browse', BrowseBusinessController::class);
@@ -23,7 +14,7 @@ Route::prefix('browse')->group(function () {
 
     Route::get('/', [BrowseBusinessController::class, 'index']);
     Route::get('/business', [BrowseBusinessController::class, 'index']);
-    // Route::get('/business/{id}/{slug?}', [BrowseBusinessController::class, 'businessShow']);
+    Route::get('/products', [BrowseProductController::class, 'index']);
 });
 
 Route::get('search-business-name/{business_name?}', function ($business_name = '') {
