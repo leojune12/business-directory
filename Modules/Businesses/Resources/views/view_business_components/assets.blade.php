dbe43fd97dbe7c819b890178ab901f28211a3323
Leo June <leojunebedeo@gmail.com>
2022-12-01 00:01:12 +0800

    Update assets file

--- Modules/Businesses/Resources/views/view_business_components/assets.blade.php
+++ Modules/Businesses/Resources/views/view_business_components/assets.blade.php
@@ -7,8 +7,8 @@
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/vue@2"></script> --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
 
